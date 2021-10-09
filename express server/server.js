b04693ddd9fed61c6abd5678700fda9689d49a6e
Sanjay <sanjaykukreti900@gmail.com>
2021-10-09 14:14:50 +0530

    ejs-template

--- express server/server.js
+++ express server/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 app.listen('3000', () => {
     console.log("server is running");
 
 })
 
 //   SETTING GLOBAL VIEW ENGINE FOR  TEMPLATES
-app.set('view engine', 'pug');
+// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' })) // HANDLEBARS
+// app.set('view engine', 'hbs')
+// app.set('view engine', 'pug'); /// PUG TEMPLATE
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +23,7 @@ app.use('/admin', exp_routes.routes);
 app.use(publicRoutes);   ////  it is comment bcz it will redirect to '/' url , so if it will not commentd
 /// then our 404 page never executed
 app.use((req, res) => {
-    res.status(404).render('page-not-found')
+    res.status(404).render('page-not-found', { pageTit: "Page Not Found :) " })
     // res.status(404).sendFile(path.join(__dirname + '/views/page-not-found.html')) /// 404 page should be in last
 })
 
