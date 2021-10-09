69caa084e357df8184fa046dffaeafd9adf4111a
Sanjay <sanjaykukreti900@gmail.com>
2021-10-09 20:10:35 +0530

    updated-shopping-cart-site

--- express server/routes/shop.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const express = require('express');
-const router = express.Router();
-const adminData = require('./admin')
-const shopP = require('../controllers/product')
-router.get('/', shopP.shopProducts)
-
-module.exports = router
