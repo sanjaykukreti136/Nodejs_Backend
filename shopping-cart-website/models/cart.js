b0770a94626b1f5ee7ea6476d8c9935d2d7e5581
Sanjay <sanjaykukreti900@gmail.com>
2021-10-11 19:02:47 +0530

    update-product-admin-side

--- shopping-cart-website/models/cart.js
+++ shopping-cart-website/models/cart.js
@@ -7,38 +7,34 @@ const p = path.join(
     'cart.json'
 );
 
-
 module.exports = class Cart {
     static addProduct(id, productPrice) {
-        /// FETCH CART FROM FILE 
+        // Fetch the previous cart
         fs.readFile(p, (err, fileContent) => {
             let cart = { products: [], totalPrice: 0 };
             if (!err) {
-                cart = JSON.parse(fileContent)
+                cart = JSON.parse(fileContent);
             }
-
-            /// ANAYLIZE THE CART 
-            const existingProductIdx = cart.products.findIndex(p => p.id === id);
-            const existingProduct = cart.products[existingProductIdx];
+            // Analyze the cart => Find existing product
+            const existingProductIndex = cart.products.findIndex(
+                prod => prod.id === id
+            );
+            const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
+            // Add new product/ increase quantity
             if (existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
                 cart.products = [...cart.products];
-                cart.products[existingProductIdx] = updatedProduct;
-
-            }
-            else {
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
                 updatedProduct = { id: id, qty: 1 };
                 cart.products = [...cart.products, updatedProduct];
             }
-
             cart.totalPrice = cart.totalPrice + +productPrice;
-            // ADD PRODUCT OR INCREASE QUANTITY
-
-            fs.writeFile(p, JSON.stringify(cart), (err) => {
+            fs.writeFile(p, JSON.stringify(cart), err => {
                 console.log(err);
-            })
-        })
+            });
+        });
     }
-}
+};
