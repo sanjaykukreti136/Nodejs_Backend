4a37a0c2846a42e771c4d15fdfd0fe01e776f229
Sanjay <sanjaykukreti900@gmail.com>
2021-10-11 20:46:12 +0530

    edit-product-details

--- shopping-cart-website/controllers/admin.js
+++ shopping-cart-website/controllers/admin.js
@@ -13,25 +13,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
-  console.log("a");
   const editMode = req.query.edit;
   if (!editMode) {
-    console.log("x");
     return res.redirect('/');
   }
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
     if (!product) {
-      console.log("p");
       return res.redirect('/');
     }
-    console.log("m");
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
@@ -39,6 +35,23 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     });
   });
+};
+
+exports.postEditProducts = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
